ea9ee86742330273c1b8d020d3fa162c4d9b5970
goffygoo <jrvineetoli52@gmail.com>
2022-04-10 18:35:41 +0530

    createServer

--- client/components/popup/createServer.js
+++ client/components/popup/createServer.js
@@ -1,14 +1,34 @@
-import styles from "../../styles/popup/CreateServer.module.css";
-import { useState } from "react";
-import axios from "axios";
+import styles from "../../styles/popup/CreateServer.module.css"
+import { useState } from "react"
+import axios from "axios"
+import Router from 'next/router'
 
 export default function createServer(props) {
   const { setView } = props;
   const [name, setName] = useState("");
 
   const click = () => {
-    // axios.
-  };
+    const user = JSON.parse(localStorage.getItem('user'))
+
+    const req = {
+      user_email: user.email,
+      server_name: name
+    }
+
+    const token = localStorage.getItem('token')
+
+    axios.post("http://192.168.1.40:5000/server/createServer", req, {
+      headers: {
+        "Authorization": token
+      }
+    }).then(res => {
+      if (res.data.success) {
+          Router.push('/server/' + res.data.server._id)
+      } else throw res.data.message
+    }).catch(err => {
+      console.log(err)
+    })
+  }
 
   return (
     <div className={styles.container}>
