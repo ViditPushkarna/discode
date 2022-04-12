cee322a533c96210522c33c1bdec772ab13d1b3c
Vidit Pushkarna <viditmanojpushkarna@gmail.com>
2022-04-12 22:52:29 +0530

    sockets running

--- server/config/chat_sockets.js
+++ server/config/chat_sockets.js
@@ -1,1 +1,14 @@
-export const chatSockets = function (chatServer) {};
+export const iofunc = (io) => {
+  io.on("connection", (socket) => {
+    console.log(`User Connected : ${socket.id}`);
+    socket.on("join_room", (data) => {
+      console.log("This one wants to join the room", data.user_name);
+      socket.join(data.chatroom);
+
+      io.in(data.chatroom).emit("user_joined", data);
+    });
+    socket.on("disconnect", () => {
+      console.log(`User disconnected : ${socket.id}`);
+    });
+  });
+};
