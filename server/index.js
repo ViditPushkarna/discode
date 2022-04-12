f0be951d4e8be7c164b2210b38c1dc56180f799c
Vidit Pushkarna <viditmanojpushkarna@gmail.com>
2022-04-12 21:23:36 +0530

    sockets in progress

--- server/index.js
+++ server/index.js
@@ -2,10 +2,34 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import routes from "./routes/index.js";
-import session from "express-session";
-import MongoStore from "connect-mongo";
+import http from "http";
+import { Server } from "socket.io";
+
 // fire up the express app
 const app = express();
+
+// chatting
+const chatServer = http.createServer(app);
+import { chatSockets } from "./config/chat_sockets.js";
+const chat_sockets = chatSockets(chatServer);
+chatServer.listen(4000, () => {
+  console.log(`Chat server running on port : ${4000}`);
+});
+
+const io = new Server(chatServer, {
+  cors: {
+    origin: ["*"],
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected : ${socket.id}`);
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected : ${socket.id}`);
+  });
+});
 
 // connect to database
 import db from "./config/mongoose.js";
